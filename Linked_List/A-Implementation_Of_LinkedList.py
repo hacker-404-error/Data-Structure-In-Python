76caea0fdf258e6dcc56e2f21f43b16331d0a521
PRITAM DAS <er.pritamdas22@gmail.com>
2021-12-19 20:43:43 +0530

    Linked_List

---
+++ Linked_List/A-Implementation_Of_LinkedList.py
@@ -0,0 +1,38 @@
+
+from os import system
+
+class node:
+
+    def __init__(self,data):
+        self.data=data
+        self.next=None
+
+class Linked_List:
+
+    def __init__(self):
+        self.head=None
+        self.Tail=None
+
+    def insert(self,element):
+        if (self.head==None and self.Tail==None):
+            self.head=node(element)
+            self.Tail=self.head
+        else:
+            self.Tail.next=node(element)
+            self.Tail=self.Tail.next
+    def display(self):
+        current=self.head
+        while(current is not None):
+            print(current.data,end="->")
+            current=current.next
+
+
+if __name__=='__main__':
+    system("cls")
+    Ll=Linked_List()
+    n=int(input("Enter the no of element you want to enter"))
+    while(n!=0):
+        element=int(input("Enter Data: "))
+        Ll.insert(element)
+        n-=1
+    Ll.display()
--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Data-Structure-Python
+
+# ARRAY
+| slno. | Programs                       |
+| ----- | ------------------------------ |
+| A.    | [Traversing.py             ]() |
+| B.    | [Min_Elment.py             ]() |
+| C.    | [Max_Element.py            ]() |
+| D.    | [Min_Max_Element.py        ]() |
+| E.    | [Insertion_At_Beg.py       ]() |
+| F.    | [Insert_At_End.py          ]() |
+| G.    | [Insert_At_Position.py     ]() |
+| H.    | [Delete_From_Beg.py        ]() |
+| I.    | [Delete_From_End.py        ]() |
+| J.    | [Delete_From_Pos.py        ]() |
+| K.    | [Linear_Serach.py          ]() |
+| L.    | [Binary_Search.py          ]() |
+| M.    | [2D_Array_Implementation.py]() |
+
+ # LINKED LIST
+ 
